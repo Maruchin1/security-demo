1090536e219ad0a5a03474262b36cedefdc9c07c
Tomasz St <niebieskicytrus@gmail.com>
2020-03-29 18:59:29 +0200

    parent UI

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import { Parent } from '../models/parent';
+import { Child } from '../models/child';
+import { Medicine } from '../models/medicine';
 
 @Component({
   selector: 'app-parent',
@@ -6,11 +9,50 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent implements OnInit {
+  parent: Parent
+  childs: Child[] = []
+  medicines: Medicine[] = []
 
   constructor() {
   }
 
   ngOnInit(): void {
+    //toDO: get some data from backend, below mocked
+    this.parent = {parentId: 11, name: "Antoni", email: "a@b.cd"}
+    this.childs.push({childId: 1, name: "Adaś", connectionKey: "UZUMYMW"})
+    this.childs.push({childId: 2, name: "Małgosia", connectionKey: "AEZAKMI"})
+    this.childs.push({childId: 3, name: "Janek", connectionKey: "44EERQQS"})
+    this.medicines.push({  medicineId: 1,
+      name: "alfa1",
+      unit: "100j",
+      expireDate: new Date("18-10-2020"),
+      packageSize: 200,
+      currState: 1})
+      this.medicines.push({  medicineId: 2,
+        name: "omega 7 Premium super fast",
+        unit: "100j",
+        expireDate: new Date("18-05-2020"),
+        packageSize: 100,
+        currState: 1})  }
+
+  addMedicine(){
+    //toDO: przekierowanie do formularza dodawania leku
+  }
+
+  deleteMedicine(medicineId: number){
+    let i = this.medicines.findIndex(c => c.medicineId === medicineId)
+    this.medicines.splice(i,1);
+    //toDO: usunac z bazy
+  }
+
+  addChild(){
+    //toDO: przekierowanie do formularza dodawania dzecka
+  }
+
+  deleteChild(childId: number){
+    let i = this.childs.findIndex(c => c.childId === childId)
+    this.childs.splice(i,1);
+    //toDO: usunac z bazy
   }
 
 }
--- src/app/parent/parent.module.ts
+++ src/app/parent/parent.module.ts
@@ -3,13 +3,17 @@ import {CommonModule} from '@angular/common';
 
 import {ParentRoutingModule} from './parent-routing.module';
 import {ParentComponent} from './parent.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [ParentComponent],
   imports: [
     CommonModule,
-    ParentRoutingModule
+    ParentRoutingModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class ParentModule {

e3389df9c8112cd7d6c15fff4eaf71306290a608
mapiekielny <mpiekielny@fream.pl>
2020-04-30 17:21:16 +0200

    Endpoint do wylogowywania

--- src/app/services/ApiEndpoints.ts
+++ src/app/services/ApiEndpoints.ts
@@ -9,6 +9,7 @@ export class ApiEndpoints {
   static readonly REGISTER_PARENT = ApiEndpoints.BASE_URL + '/users/register-parent';
   static readonly LOGIN_PARENT = ApiEndpoints.BASE_URL + '/users/login-parent';
   static readonly LOGIN_CHILD = ApiEndpoints.BASE_URL + '/users/login-child';
+  static readonly LOGOUT = ApiEndpoints.BASE_URL + '/users/logout';
   static readonly USER_ROLE = ApiEndpoints.BASE_URL + '/users/role';
   static readonly GET_LOGGED_CHILDREN = ApiEndpoints.BASE_URL + '/children/byAuthToken';
   static readonly GET_CHILD_MEDICINES = ApiEndpoints.BASE_URL + '/childMedicines';
